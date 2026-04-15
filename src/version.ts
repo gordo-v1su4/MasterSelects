22d0c04082d36283142f4398576e9a1224cbe655
Sportinger <roman.kuskowski@googlemail.com>
2026-04-15 18:33:34 +0200

    Bump version to 1.5.2 for release

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // App version
 // Format: MAJOR.MINOR.PATCH
-export const APP_VERSION = '1.5.1';
+export const APP_VERSION = '1.5.2';
 
 export interface ChangelogNotice {
   type: 'info' | 'warning' | 'success' | 'danger';
